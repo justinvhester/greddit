3ed7a9ad1581279bd152859964ec97697ab3ac6c
Justin Hester <justinvhester@users.noreply.github.com>
2015-08-27 11:21:23 -0500

    Title and number of comments as well as URL now included in output.

--- main.go
+++ main.go
@@ -1,44 +1,68 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"errors"
+	"log"
+	"net/http"
 )
 
 func main() {
-    resp, err := http.Get("http://reddit.com/r/golang.json")
-    if err != nil {
-        log.Fatal(err)
-    }
-    if resp.StatusCode != http.StatusOK {
-        log.Fatal(resp.Status)
-    }
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    type Item struct {
-        Title string
-        URL   string
-    }
-
-    type Response struct {
-        Data struct {
-            Children []struct {
-                Data Item
-            }
-        }
-    }
-
-    r := new(Response)
-    err = json.NewDecoder(resp.Body).Decode(r)
-
-    for _, child := range r.Data.Children {
-        fmt.Println(child.Data.Title)
-    }
-
+	items, err := Get("golang")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range items {
+		fmt.Println(item)
+	}
 }
 
+type Response struct {
+	Data struct {
+		Children []struct {
+			Data Item
+		}
+	}
+}
 
+type Item struct {
+	Title    string
+	URL      string
+	Comments int `json:"num_comments"`
+}
+
+func (i Item) String() string {
+	com := ""
+	switch i.Comments {
+	case 0:
+		// nothing
+	case 1:
+		com = " (1 comment)"
+	default:
+		com = fmt.Sprintf(" (%d comments)", i.Comments)
+	}
+	return fmt.Sprintf("%s%s\n%s", i.Title, com, i.URL)
+}
+
+func Get(reddit string) ([]Item, error) {
+	url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	r := new(Response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	items := make([]Item, len(r.Data.Children))
+	for i, child := range r.Data.Children {
+		items[i] = child.Data
+	}
+	return items, nil
+}
